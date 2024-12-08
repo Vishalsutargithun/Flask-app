a76741b95d36b3734faeb4842b2eca283c8d6489
Vishal <160883375+Vishalsutargithun@users.noreply.github.com>
2024-12-08 13:48:04 +0530

    Update flask-function.py

--- api/flask-function.py
+++ api/flask-function.py
@@ -1,15 +1,10 @@
-from flask import Flask, request
-
-from vercel import Vercel
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
+@app.route("/")
+def home():
+    return jsonify({"message": "Hello, Vercel!"})
 
-return 'Hello, World!'
-
-if __name__ == '__main__':
-
-app.run()
-
+if __name__ == "__main__":
+    app.run()
